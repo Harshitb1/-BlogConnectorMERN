3d887273226d6fdb6f787468d63ac2389f4c6fa4
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-06-30 12:52:08 +0530

    post,comment added

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -6,8 +6,8 @@ class ProfileGithub extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clientId: '26c196bacea7db10cf48',
-      clientSecret: '0885cb690e07d2a93a6afb0891fb552fd9f7aa53',
+      clientId: '2e1d577d859210df3282',
+      clientSecret: 'e37af8dcd10d51ece94ecebf14f3db499fd259bf',
       count: 5,
       sort: 'created: asc',
       repos: []
